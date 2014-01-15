4a2c805c6cc2c79d8a02958eba2074afa28da8e4
Hogar <Hogar.fox@gmail.com>
2014-01-15 18:27:29 +0300

    remove news when delete

--- server/core/models/UserModel.php
+++ server/core/models/UserModel.php
@@ -417,6 +417,17 @@
                 $response->setCode(Response::CODE_ERROR)->setError($err[2]);
             }
 
+            $sql = 'DELETE FROM user_news WHERE user_id = :user_id';
+            $query = $db->prepare($sql);
+            $query->execute(array(
+                ':user_id' => $userId
+            ));
+
+            $err = $query->errorInfo();
+            if($err[1] != null){
+                $response->setCode(Response::CODE_ERROR)->setError($err[2]);
+            }
+
             return $response;
         }
 
