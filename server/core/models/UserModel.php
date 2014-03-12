c91b0e42051050f8b0398eb8bb348ddfd1e9d65d
Hogar <Hogar.fox@gmail.com>
2014-03-12 14:16:33 +0300

    delete user with clear 'user_award_list', 'user_mission_list'

--- server/core/models/UserModel.php
+++ server/core/models/UserModel.php
@@ -533,6 +533,26 @@
                 $response->setCode(Response::CODE_ERROR)->setError($err[2]);
             }
 
+            $sql = 'DELETE FROM user_mission WHERE user_id = :user_id';
+            $query = $db->prepare($sql);
+            $query->execute(array(
+                ':user_id' => $userId
+            ));
+            $err = $query->errorInfo();
+            if($err[1] != null){
+                $response->setCode(Response::CODE_ERROR)->setError($err[2]);
+            }
+
+            $sql = 'DELETE FROM user_award WHERE user_id = :user_id';
+            $query = $db->prepare($sql);
+            $query->execute(array(
+                ':user_id' => $userId
+            ));
+            $err = $query->errorInfo();
+            if($err[1] != null){
+                $response->setCode(Response::CODE_ERROR)->setError($err[2]);
+            }
+
             return $response;
         }
 
